30f5fe0bb496ffde04e80ed095461b095a615e17
PedroBGSilva <pedrobernardosilva@hotmail.com>
2024-10-10 18:47:08 +0100

    Created services folder

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -7,7 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
-import { AuthService } from '../auth.service';
+import { AuthService } from '../services/auth.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { ResetPasswordComponent } from '../reset-password/reset-password.component';
 
