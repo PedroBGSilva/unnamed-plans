9c212ee2b75538a93e635238b6543b74f6d9eb9b
Bruno <colaco.brunoc@gmail.com>
2024-10-10 18:40:34 +0100

    Added persistent sign in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { firebaseConfig } from 'src/environments/environment';
 
 @Component({
@@ -9,11 +11,24 @@ import { firebaseConfig } from 'src/environments/environment';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private translate: TranslateService) {
+
+  constructor(private translate: TranslateService,private router: Router) {
     this.translate.addLangs(['en']);
     this.translate.setDefaultLang('en');
     // const browserLang = this.translate.getBrowserLang();
     // this.translate.use(browserLang?.match(/en|fr/) ? browserLang : 'en');
+
     initializeApp(firebaseConfig);
+    const auth = getAuth();
+    
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is logged in, navigate to home page
+        this.router.navigate(['home']);
+      } else {
+        // No user is logged in, navigate to login page
+        this.router.navigate(['login']);
+      }
+    });
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, setPersistence, browserLocalPersistence, inMemoryPersistence} from 'firebase/auth';
 
 @Injectable({
     providedIn: 'root',
@@ -9,13 +9,21 @@ export class AuthService {
     private auth = getAuth();
 
     login(email: string, password: string) {
-        return new Promise((resolve, reject) => {
-            signInWithEmailAndPassword(this.auth, email, password)
-                .then((result) => {
-                    resolve(result);
-                }).catch((error) => {
-                    reject(error);
-                })
-        })
+
+        return setPersistence(this.auth, browserLocalPersistence)
+            .then(() => {
+                
+                // In memory persistence will be applied to the signed in Google user
+                // even though the persistence was set to 'none' and a page redirect
+                // occurred.
+                return signInWithEmailAndPassword(this.auth, email, password)
+                        
+                
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
     }
 }
