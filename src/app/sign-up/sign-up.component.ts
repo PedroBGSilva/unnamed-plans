da0c93a887d6e121b6f3f04c916210e03e2c425d
PedroBGSilva <pedrobernardosilva@hotmail.com>
2025-01-08 22:06:00 +0000

    Added birthday to signup

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -4,21 +4,24 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss'],
+  styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
   @Output() signup = new EventEmitter();
 
   dismissButton = [{ text: '', role: 'cancel' }];
+  calendarToggled: boolean = false;
   showToast: boolean = false;
   toastMessage: string = '';
-  password2: string = '';
-  password: string = '';
-  email2: string = '';
-  email: string = '';
-  firstName: string = '';
-  lastName: string = '';
-  birthday: Date = new Date();
+  profile = {
+    firstName: '',
+    lastName: '',
+    birthday: new Date().toISOString(),
+    email: '',
+    email2: '',
+    password: '',
+    password2: ''
+  }
 
   constructor(
     private translateService: TranslateService
@@ -27,21 +30,25 @@ export class SignUpComponent implements OnInit {
   ngOnInit() { }
 
   regist() {
-    if (this.email !== this.email2) {
+    if (this.profile.email !== this.profile.email2) {
       this.toastMessage = this.translateService.instant('SIGNUP.EMAIL_ERROR');
       this.showToast = true;
-    } else if (this.password !== this.password2) {
+    } else if (this.profile.password !== this.profile.password2) {
       this.toastMessage = this.translateService.instant('SIGNUP.PASSWORD_ERROR');
       this.showToast = true;
     } else {
       this.signup.emit({
-        email: this.email,
-        password: this.password,
-        firstName: this.firstName,
-        lastName: this.lastName,
-        birthday: this.birthday
+        email: this.profile.email,
+        password: this.profile.password,
+        firstName: this.profile.firstName,
+        lastName: this.profile.lastName,
+        birthday: new Date(this.profile.birthday)
       });
     }
+  }
+
+  toggleCalendar() {
+    this.calendarToggled = !this.calendarToggled;
   }
 
 }
