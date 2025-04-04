2b6d55c49059b88769d39c3db77e2cf2f7bf8291
PedroBGSilva <pedrobernardosilva@hotmail.com>
2025-04-04 15:51:07 +0100

    Added communities as future implementation

---
+++ src/app/components/communities/communities.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'communities',
+  templateUrl: './communities.component.html',
+  styleUrls: ['./communities.component.scss'],
+})
+export class CommunitiesComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
